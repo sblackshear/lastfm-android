c5fac840822f0533e9c7116291670ef3db4a43b2
Sam Steele <sam@last.fm>
2011-05-05 11:39:56 -0700

    Capitalize the RadioTracks built by Boffin

--- app/src/fm/last/android/db/LocalCollection.java
+++ app/src/fm/last/android/db/LocalCollection.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import fm.last.android.LastFMApplication;
 import fm.last.api.RadioTrack;
@@ -484,9 +485,38 @@ public class LocalCollection extends SQLiteOpenHelper
         public File file;
         public FileMeta meta;
         public float weight;
+        
+        private String cap( String s ) {
+        	for (int i = 0; i < s.length(); i++) {
+
+                if (i == 0) {
+                    // Capitalize the first letter of the string.
+                    s = String.format( "%s%s",
+                                 Character.toUpperCase(s.charAt(0)),
+                                 s.substring(1) );
+                }
+
+                // Is this character a non-letter or non-digit?  If so
+                // then this is probably a word boundary so let's capitalize
+                // the next character in the sequence.
+                if (!Character.isLetterOrDigit(s.charAt(i))) {
+                    if (i + 1 < s.length()) {
+                        s = String.format( "%s%s%s",
+                                     s.subSequence(0, i+1),
+                                     Character.toUpperCase(s.charAt(i + 1)),
+                                     s.substring(i+2) );
+                    }
+                }
+
+            }
+                
+            return s;
+                        
+        }
+        
         public RadioTrack toRadioTrack() {
         	//	public RadioTrack(String locationUrl, String title, String identifier, String album, String creator, String duration, String imageUrl, String trackAuth, Boolean loved, String[] context) {
-        	return new RadioTrack(file.name(), meta.m_title, "", meta.m_album, meta.m_artist, String.valueOf(meta.m_duration), "", "", false, null);
+        	return new RadioTrack(file.name(), cap(meta.m_title), "", cap(meta.m_album), cap(meta.m_artist), String.valueOf(meta.m_duration), "", "", false, null);
         }
     };
 }
