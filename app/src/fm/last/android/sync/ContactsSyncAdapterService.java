f99f5f51bb1577f2d74bd3263c7127b113d4a488
Sam Steele <sam@last.fm>
2012-02-27 10:13:25 -0800

    Bump version number to 1.9.7

--- app/src/fm/last/android/sync/ContactsSyncAdapterService.java
+++ app/src/fm/last/android/sync/ContactsSyncAdapterService.java
@@ -301,10 +301,6 @@ public class ContactsSyncAdapterService extends Service {
 			}
 		}
 
-		Log.i("Sync", "DB Schema: " + PreferenceManager.getDefaultSharedPreferences(LastFMApplication.getInstance()).getInt("sync_schema", 0));
-		if(is_full_sync)
-			Log.i("Sync", "Performing full sync");
-		
 		Editor editor = PreferenceManager.getDefaultSharedPreferences(LastFMApplication.getInstance()).edit();
 		editor.remove("do_full_sync");
 		editor.putInt("sync_schema", syncSchema);
